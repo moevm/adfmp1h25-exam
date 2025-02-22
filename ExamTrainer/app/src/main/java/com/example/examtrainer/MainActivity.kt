d62b6eeb2943f4248fe4ca6d54d0cf6c36583863
SharinAlex <alex.shavrin200303@gmail.com>
2025-02-22 13:07:43 +0300

    add navigation graph in main activity

--- ExamTrainer/app/src/main/java/com/example/examtrainer/MainActivity.kt
+++ ExamTrainer/app/src/main/java/com/example/examtrainer/MainActivity.kt
@@ -5,6 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.compose.ExamTrainerTheme
+import com.example.examtrainer.presentation.ui.MainScreen
+import com.example.examtrainer.presentation.navigation.NavGraph
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +24,16 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setContent {
+            ExamTrainerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
+                }
+            }
         }
     }
 }
