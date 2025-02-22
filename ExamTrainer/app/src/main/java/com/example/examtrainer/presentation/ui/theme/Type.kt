0133a6505057c793d2dc24e3e2534d23f974b8c6
SharinAlex <alex.shavrin200303@gmail.com>
2025-02-22 12:20:55 +0300

    add theme (first variant)

---
+++ ExamTrainer/app/src/main/java/com/example/examtrainer/presentation/ui/theme/Type.kt
@@ -0,0 +1,54 @@
+package com.example.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val ExamTrainerTypography = Typography(
+    displayLarge = TextStyle(
+        fontSize = 36.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    displayMedium = TextStyle(
+        fontSize = 34.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    displaySmall = TextStyle(
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    titleLarge = TextStyle(
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    titleMedium = TextStyle(
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    titleSmall = TextStyle(
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    bodyLarge = TextStyle(
+        fontSize = 24.sp
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 22.sp
+    ),
+    bodySmall = TextStyle(
+        fontSize = 20.sp
+    ),
+    labelLarge = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Light
+    ),
+    labelMedium = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Light
+    ),
+    labelSmall = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Light
+    ),
+)
