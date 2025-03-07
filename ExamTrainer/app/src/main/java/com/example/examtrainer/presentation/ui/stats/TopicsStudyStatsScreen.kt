8e03d051b162f72f13f05e89523efe2f57658f90
YariKar <kyaroslav03@gmail.com>
2025-03-07 17:00:55 +0300

    add sorting topics

--- ExamTrainer/app/src/main/java/com/example/examtrainer/presentation/ui/stats/TopicsStudyStatsScreen.kt
+++ ExamTrainer/app/src/main/java/com/example/examtrainer/presentation/ui/stats/TopicsStudyStatsScreen.kt
@@ -17,6 +17,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -45,6 +50,18 @@ val topics = listOf(
 
 @Composable
 fun TopicsStudyStatsScreen(navController: NavController){
+    var sortAscending by remember { mutableStateOf(true) }
+    val sortedTopics by remember(sortAscending) {
+        derivedStateOf {
+            if (sortAscending) {
+                topics.sortedBy { it.progress }
+            } else {
+                topics.sortedByDescending { it.progress }
+            }
+        }
+    }
+    
+
     Column(
         modifier = Modifier
             .fillMaxSize()
