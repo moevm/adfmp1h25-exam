5b71aadba7c2c02ef8a4852ec265f71ce271eed2
YariKar <kyaroslav03@gmail.com>
2025-03-07 18:30:00 +0300

    add title

--- ExamTrainer/app/src/main/java/com/example/examtrainer/presentation/ui/stats/TopicsStudyStatsScreen.kt
+++ ExamTrainer/app/src/main/java/com/example/examtrainer/presentation/ui/stats/TopicsStudyStatsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
@@ -13,7 +14,10 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.examtrainer.presentation.navigation.NavRoutes
@@ -60,7 +65,7 @@ fun TopicsStudyStatsScreen(navController: NavController){
             }
         }
     }
-    
+
 
     Column(
         modifier = Modifier
@@ -89,4 +94,23 @@ fun TopicsStudyStatsScreen(navController: NavController){
             )
         }
         }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Изученность тем",
+            style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+            modifier = Modifier.weight(1f)
+        )
+
+        IconButton(onClick = { sortAscending = !sortAscending }) {
+            Icon(
+                imageVector = if (sortAscending) Icons.Default.ArrowUpward else Icons.Default.ArrowDownward,
+                contentDescription = "Сортировка"
+            )
+        }
+    }
 }
