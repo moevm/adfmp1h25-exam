ed2eeb29c04ab3c3c44dd4909efe4db14d16f604
YariKar <kyaroslav03@gmail.com>
2025-03-07 16:27:45 +0300

    add topics study page file

---
+++ ExamTrainer/app/src/main/java/com/example/examtrainer/presentation/ui/stats/TopicsStudyStatsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.examtrainer.presentation.ui.stats
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun TopicsStudyStatsScreen(navController: NavController){
+
+}
