3d67b286637af5b3e73d30389c1c5b59768701bf
YariKar <kyaroslav03@gmail.com>
2025-03-16 15:32:05 +0300

    stats repository: add fun defines

--- ExamTrainer/app/src/main/java/com/example/examtrainer/data/local/StatsRepository.kt
+++ ExamTrainer/app/src/main/java/com/example/examtrainer/data/local/StatsRepository.kt
@@ -2,6 +2,10 @@ package com.example.examtrainer.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.example.examtrainer.R
+import com.example.examtrainer.domain.model.ExamItem
+import com.example.examtrainer.domain.model.StatisticData
+import com.example.examtrainer.domain.model.Topic
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -9,17 +13,25 @@ class StatsRepository @Inject constructor(
     private val sharedPreferences: SharedPreferences,
     @ApplicationContext private val context: Context
 ) {
+    companion object {
+        private const val SELECTED_EXAM_KEY = "selected_exam"
+    }
 
+    // Получаем выбранный экзамен из SharedPreferences
+    fun getSelectedExam(): ExamItem? {
+        val selectedExamName = sharedPreferences.getString(SELECTED_EXAM_KEY, null)
+        return selectedExamName?.let { ExamItem(it.hashCode(), it) }
+    }
     //TODO
-    fun getAttendance() :List<Boolean>{
-        return mutableListOf();
+    fun getAttendance() :Map<String, Boolean>{
+        val inputStream =  context.resources.openRawResource(R.raw.stats)
     }
 
-    fun getGeneralStats(){
+    fun getGeneralStats(): StatisticData {
 
     }
 
-    fun getTopicsStats(){
+    fun getTopicsStats(): List<Topic>{
         //context.resources
     }
 
